d78736382475bdba58fcb21f7b60a34285e3a70b
kingwell47 <joeldoctor47@gmail.com>
2021-08-30 13:38:17 +0800

    did corrections based on pikamart and FEM's suggestions

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,2 @@
-const btnHamburger = document.querySelector("#btnHamburger");
-const body = document.querySelector("body");
-const btnMenu = document.querySelector(".header__menu");
-
-btnHamburger.addEventListener('click', function(){
-    if (btnHamburger.classList.contains('open')) {
-        btnHamburger.classList.remove('open');
-        btnMenu.classList.add("hidden");
-        body.classList.remove("noscroll");
-    } else {    
-        btnHamburger.classList.add('open');
-        btnMenu.classList.remove("hidden");
-        body.classList.add("noscroll");
-    }
-})
+"use strict";var btnHamburger=document.querySelector("#btnHamburger"),body=document.querySelector("body"),btnMenu=document.querySelector(".header__menu");btnHamburger.addEventListener("click",(function(){btnHamburger.classList.contains("open")?(btnHamburger.classList.remove("open"),btnMenu.classList.add("hidden"),body.classList.remove("noscroll")):(btnHamburger.classList.add("open"),btnMenu.classList.remove("hidden"),body.classList.add("noscroll"))}));
+//# sourceMappingURL=script.js.map
